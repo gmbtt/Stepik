d18701a7ca20a974bd7e556f2075189b6f8e8bda
Sviatoslav Alekseev <sviatoslav.alekseev@nordigy.ru>
2021-03-22 04:05:58 +0500

    Fix indentation. Some Decoder refactor.

--- Decoder.py
+++ Decoder.py
@@ -1,28 +1,31 @@
-with open('input.txt', 'r') as inf:
-    s = inf.readline().strip()
+"""
+This is decoder.
+Example: #TODO
+"""
+
+with open('input.txt', 'r') as infile:
+    s = infile.read().strip()
 
 a = ''
 b = ''
 i = 0
-j = 0
 number = ''
 
-for i in range(len(s)):
-    if s[i] >= 'A':
-        b = s[i]
-    elif s[i] < 'A':
-        number += s[i]
+for i, char in enumerate(s):
+    if char >= 'A':
+        b = char
+    else:
+        number += char
         if i == len(s)-1:
-            while j < int(number):
-                    a += b
-                    j += 1
+            for _ in range(int(number)):
+                a += b
+
         else:
             if s[i+1] >= 'A':
-                while j < int(number):
+                for _ in range(int(number)):
                     a += b
-                    j += 1
-                number = ''
-                j = 0
 
-with open('output.txt', 'w') as ouf:
-    ouf.write(a)
+                number = ''
+
+with open('output.txt', 'w') as outfile:
+    outfile.write(a)
--- Encoder.py
+++ Encoder.py
@@ -1,16 +1,26 @@
+"""
+This is encoder.
+Example: #TODO
+"""
+
 a = input()
 b = ''
 i = 0
 j = len(a)
+
 if j == 1:
     print(a + str(1))
+
 while i+1 < j:
     n = 1
     while i+1 < j and a[i] == a[i+1]:
         n += 1
         i += 1
+
     b = b + str(a[i]) + str(n)
     i += 1
+
 if j > 1 and a[-2] != a[-1]:
     b = b + str(a[-1]) + str(1)
+
 print(b)
